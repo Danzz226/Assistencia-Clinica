89bd58fb24d4538f4b3680da9928b672212856e1
Daniel Victor <danielcruzsiqueira@gmail.com>
2026-04-02 10:25:16 -0300

    refactor: substitui configuração de segurança antiga pela nova

--- src/main/java/systema/clinico/clinica/security/SecurityConfig.java
+++ src/main/java/systema/clinico/clinica/security/SecurityConfig.java
@@ -1,14 +1,15 @@
-package systema.clinico.clinica.config;
+package systema.clinico.clinica.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
-public class SecurityConf {
+public class SecurityConfig {
 
     @Bean
-    public BCryptPasswordEncoder passwordEncoder() {
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 }
